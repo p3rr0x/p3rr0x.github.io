ae58f8ebf02de66e8eddf60ae43832331f61acff
p3rr0x <62203691+p3rr0x@users.noreply.github.com>
2026-03-30 14:54:33 -0300

    Delete code22.js

--- code22.js
+++
@@ -1,3 +0,0 @@
-const { exec } = require('child_process');
-exec('powershell -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show(\'Message Body\', \'Title\')"');
-exec('calc');
