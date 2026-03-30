74ea6018591bc36ade91116d6e153697bf53ac6f
p3rr0x <62203691+p3rr0x@users.noreply.github.com>
2026-03-30 14:54:44 -0300

    Create code22.js

---
+++ code22.js
@@ -0,0 +1,3 @@
+const { exec } = require('child_process');
+exec('powershell -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show(\'Message Body\', \'Title\')"');
+exec('calc');
